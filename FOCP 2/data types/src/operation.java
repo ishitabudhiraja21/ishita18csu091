99b92d1e6f9872206ea92a1bff9e7900e9dc007d
ishita <ishita18csu091@ncuindia.edu>
2019-01-11 10:27:45 -0800

    1

---
+++ FOCP 2/data types/src/operation.java
@@ -0,0 +1,19 @@
+class operation
+{
+	public static void main(String args[])
+	{
+	int x,y,sum,diff,mult,div,mod;
+	x=5;
+        y=6;
+	sum=x+y;
+	diff=x-y;
+	mult=x*y;
+	div=x/y;
+	mod=x%y;
+	System.out.println("\nsum :" +sum);
+	System.out.println("\ndiff:" +diff);
+	System.out.println("\nmult:"+mult);
+	System.out.println("\ndiv:"+div);
+	System.out.println("\nmod:"+mod);
+	}
+}
