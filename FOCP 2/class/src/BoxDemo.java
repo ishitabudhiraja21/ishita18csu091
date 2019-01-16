3ecc8b5ad7913b8ad6a9959bf775701e35541ae6
ishita <ishita18csu091@ncuindia.edu>
2019-01-15 16:26:55 -0800

    2

--- FOCP 2/class/src/BoxDemo.java
+++ FOCP 2/class/src/BoxDemo.java
@@ -9,7 +9,20 @@ class Box
 	height=10;
 	depth=4;
 	}
+	Box(double w,double h,double d)
+	{
+		this.width=w;
+		this.heigth=h;
+		this.depth=d;
+	}
+	Box()
+	{
+	this.width=0;
+	this.heigth=0;
+	this.depth=0;
+	}
 	double volume()
+
 	{
 	double volume=width*height*depth;
 	return volume;
@@ -20,10 +33,13 @@ class BoxDemo
 {
 	public static void main(String args [])
 	{
-	Box myBox=new Box();
+	Box myBox=new Box(4,5,6);
+    double vol=myBox.volume();
+	System.out.println("volume after using constructors:"=vol);
 	myBox.setDim();
 	double volume=myBox.volume();
 	System.out.println("volume:"+volume);
-
+     
+     System.out.println("volume after using constructors:"=volume);
 	}
 }
